63ad74e44687e4f90b4461543a3787b8cb0b1557
Saulo Frauches <saulo_pf@hotmail.com>
2021-08-10 12:01:24 -0300

    ajustando o modelo de front-end

--- app/controllers/http/HomeDashBoardController.php
+++ app/controllers/http/HomeDashBoardController.php
@@ -7,7 +7,22 @@ class HomeDashBoardController
     
     public function index()
     {
-        require './app/views/templates/basicDashBoardTemplate.php';
+        
+        $template = file_get_contents ("./app/views/templates/basicDashBoardTemplate.php"); 
+        
+        //require './app/views/templates/basicDashBoardTemplate.php';
+                
+        ob_start();        
+        require './app/views/home/index.php';
+        $saida = ob_get_contents();
+        ob_end_clean();
+        
+        $find = ["{{backendPHP}}"];
+        $replace = [$saida];
+        
+        $printTemplate = str_replace($find, $replace, $template);
+        echo $printTemplate;
+        
     }
     
 }
